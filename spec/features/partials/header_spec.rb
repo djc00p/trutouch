2171f81fff91b8a2e50e59e8e2f3fc1136760993
Deonte Cooper <deonte.cooper12@gmail.com>
2021-08-23 00:31:00 -0600

    Add Nav Partial testing

---
+++ spec/features/partials/header_spec.rb
@@ -0,0 +1,115 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Header", type: :feature do
+  before do
+    visit root_path
+  end
+
+  describe "Logo" do
+    it "has an image tag" do
+      within ".logo" do
+        expect(page).to have_css("img", id: "logo")
+      end
+    end
+
+    it "is a link to the home page" do
+      visit products_used_path
+
+      click_on "TruTouch Logo"
+
+      expect(page).to have_current_path(root_path, ignore_query: true)
+    end
+  end
+
+  describe "Nav" do
+    it "has a link to the Home page" do
+      within ".nav_links" do
+        expect(page).to have_link("Home")
+      end
+    end
+
+    it "has a link to the CORONAVIRUS/COVID-19 page" do
+      within ".nav_links" do
+        expect(page).to have_link("Covid-19")
+      end
+    end
+
+    it "has a link to the Our Services page" do
+      within ".nav_links" do
+        expect(page).to have_link("Our Services")
+      end
+    end
+
+    it "has a link to the Products Used page" do
+      within ".nav_links" do
+        expect(page).to have_link("Products Used")
+      end
+    end
+
+    it "has a link to the Portfolio page" do
+      within ".nav_links" do
+        expect(page).to have_link("Portfolio")
+      end
+    end
+
+    it "has a link to the Appointment page" do
+      within ".nav_links" do
+        expect(page).to have_link("Appointment")
+      end
+    end
+
+    it "has a link to the Contact Us page" do
+      within ".nav_links" do
+        expect(page).to have_link("Contact Us")
+      end
+    end
+  end
+
+  describe "Nav Links Functionality" do
+    it "links to the Home page" do
+      visit "covid_19"
+
+      click_on "Home"
+
+      expect(page).to have_current_path(root_path, ignore_query: true)
+    end
+
+    it "links to the CORONAVIRUS/COVID-19 page" do
+      click_on "Covid-19"
+
+      expect(page).to have_current_path(covid_19_path, ignore_query: true)
+    end
+
+    it "links to the Our Services page" do
+      click_on "Our Services"
+
+      expect(page).to have_current_path(our_services_path, ignore_query: true)
+    end
+
+    it "links to the Products Used page" do
+      click_on "Products Used"
+
+      expect(page).to have_current_path(products_used_path, ignore_query: true)
+    end
+
+    it "links to the Portfolio page" do
+      click_on "Portfolio"
+
+      expect(page).to have_current_path(portfolio_path, ignore_query: true)
+    end
+
+    it "links to the Appointment page" do
+      click_on "Appointment"
+
+      expect(page).to have_current_path(appointment_path, ignore_query: true)
+    end
+
+    it "links to the Contact Us page" do
+      click_on "Contact Us"
+
+      expect(page).to have_current_path(contact_us_path, ignore_query: true)
+    end
+  end
+end
