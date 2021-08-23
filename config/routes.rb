ae5cd50ac0c36b20c55b39ca75430ec93ef872f5
Deonte Cooper <deonte.cooper12@gmail.com>
2021-08-23 00:19:34 -0600

    Add navigation routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root "home#index"
+  root 'home#index'
+
+  get '/covid_19', to: 'covid_19#index'
+  get '/our_services', to: 'our_services#index'
+  get '/products_used', to: 'products_used#index'
+  get '/portfolio', to: 'portfolio#index'
+  get '/appointment', to: 'appointment#index'
+  get '/contact_us', to: 'contact_us#index'
 end
