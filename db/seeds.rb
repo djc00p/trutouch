476a7a371d089175a5f68ecb579be897cff1f4de
Deonte Cooper <deonte.cooper12@gmail.com>
2021-11-16 11:38:02 -0700

    Seed data for DetailService

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,132 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([{ name: 'Star Wars' },, { name: 'Lord of the Rings' },])
 #   Character.create(name: 'Luke', movie: movies.first)
+DetailService.create([
+  {
+    name: "Full Detail",
+    base_price: 320,
+    description: "With our Full Detail package you will recieve both an Interior and Exterior detail."
+  },
+  {
+    name: "Executive Full Detail",
+    base_price: 370,
+    description: "With our Executive Full Detail package you receive everything that comes with the Full Detail package, alonge with a premium carnuba wax sealant. This longer lasting sealant, protects your clear coat from the elements."
+  },
+  {
+    name: "Presidential Full Detail",
+    base_price: 420,
+    description: "With our Presidential Full Detail package you receive everything that comes with the Full Detail Package, alonge with a ultra-premium patriot wax sealant. This longer lasting sealant, protects the clear coat from the elements. Finalized with a Brilliant Glaze, adding amazing depth and shine."
+  },
+  {
+    name: "Interior Detail",
+    base_price: 210,
+    description: "Our Interior Detail package includes an in depth cleaning of the following; seats, console, headliners, inside door panels, carpet, dressing (give it a shine), vacuum, shampoo. Excludes trunks unless requested (must be empty). Remove all personal items and trash before detail. Cost could increase upon inspection."
+  },
+  {
+    name: "Exterior Detail",
+    base_price: 210,
+    description: "Our Exterior Detail package includes an in depth cleaning of the following; the engine bay, an ultra foam shampoo exterior wash, spraying off tires and rims plus tire dressing, minor paint correction, door jams, windows outside, exterior trim revitalization, Polish and butter wax finish."
+  },
+  {
+    name: "High-End Wash/Wax Revitalization",
+    base_price: 130,
+    description: "Our High-End Wash/Wax Revitalization package includes an ultra foam shampoo exterior wash, spraying off tires and rims plus tire dressing, a revitalization hand polish, and then finished off with Adams detail spray high gloss finish."
+  },
+  {
+    name: "High-End WWR and Vac",
+    base_price: 175,
+    description: "Our High-End Wash/Wax Revitalization package includes an ultra foam shampoo exterior wash, spraying off tires and rims plus tire dressing, a revitalization hand polish, and then finished off with Adams detail spray high gloss finish. Vacuum floors and seats. (DOG HAIR REMOVAL NOT INCLUDED. NO INTERIOR WIPE DOWNS)."
+  },
+  {
+    name: "Touch Up Scratch and Buff",
+    base_price: 50,
+    description: "Wet sanding and buffing. If the scratch is to deep, paint blending would be required in order to remove scratch, which will be extra. An evaluation of scratches is needed to assess appropriate pricing. Adjustments based on size of vehicle and number of areas being worked on will increase the price."
+  },
+  {
+    name: "Scratch and Buff Panel Webbing",
+    base_price: 800,
+    description: "Includes standard scratch and buff but also focus on webbing removal across full panel. Can discuss single panel work. Please bring up when you call or if an appointment inquiry is made via the website."
+  },
+  {
+    name: "Hazardous Waste Management",
+    base_price: 135,
+    description: "Hazardous waste can be dangerous and requires a special removal process. Our goal is ensure proper removal, decontamination, and sanitizing of hazard. This makes the environment safe for consumers. Below are a list of items considered hazardous:
+    Urine (animal or human),
+    Feces (animal or human),
+    Mold,
+    Blood,
+    Vomit
+
+    Please note excessive hazardous waste will have an up charge."
+  },
+  {
+    name: "Steam Cleaning",
+    base_price: 75,
+    description: "Steamer is used inorder to sterilize interior panels, steering wheel, front dash, leather seats, exterior. All surface areas that can withstand 175 to 250 degrees heat."
+  },
+  {
+    name: "Odor & Disinfection Bomb",
+    base_price: 130,
+    description: "Used in interior to kill germs, bacteria, mold/mildew, and viruses.  Eliminates odors (smoke, pets, and many others).   This product is industrial strength incorporates Chlorine Dioxide used for 50 yrs in various industries incl. surgical equipment sterilization.
+
+    Available in a details with interiors for $95 dollars
+    Recommend one uses the bomb with an interior detail to ensure all smells disappear / safety measures take hold
+    Recommend one uses w/ steam cleaning service to double the protection"
+  },
+  {
+    name: "Head-Light Restoration",
+    base_price: 85,
+    description: "Two stage wet sanding and polish seal."
+  },
+  {
+    name: "Tail-Light Restoration",
+    base_price: 85,
+    description: "Two stage wet sanding and polish seal"
+  },
+  {
+    name: "Steam Engine Cleaning",
+    base_price: 85,
+    description: "Steam cleaned, using an high quality all purpose eco degreasing agent"
+  },
+  {
+    name: "Shampoo Service",
+    base_price: 85,
+    description: "High-end Shampoo Service cost determined upon inspection. Using a carpet upholstery cleaner that knocks the worst soil and stains.  We can shampoo the seats, floors, and mats. Each area is an addition charge. Excessive staining could result in increased cost."
+  },
+  {
+    name: "Dirt, Dog Hair, or Sand Removal - No Detail Basic",
+    base_price: 120,
+    description: "This service is for if an interior detail is not wanted but dirt, dog hair, or sand removal is required. Please note excessive amount of any dirt, dog hair, or sand is an upcharge."
+  },
+  {
+    name: "Excessive Dirt, Hair, or Sand Removal",
+    base_price: 79,
+    description: "Excessive Dirt, Hair, or Sand Removal cost may increase based amount of dirt, hair, or sand inside vehicle."
+  },
+  {
+    name: "Ceramic Body Protection Package",
+    base_price: 700,
+    description: "Includes excellent exterior protection that shows great scratch resilience and unbeatable water beading properties. This service does require 1 to 3 days depending on the number of years coating protection being requested. Can see adjustments based on size and condition of vehicle paint.  Must be inspected in person. Does not include major scratch removal or repair."
+  },
+  {
+    name: "Exterior Car Wash",
+    base_price: 20,
+    description: "A quick hand wash after a rain or snow storm.
+    Includes for all vehicle types:
+    Hand wash with our high-end shampoo that includes a wax coating
+    Dry off exterior
+    Not available with this package:
+    Interior work
+    Tire dressing
+    Windows
+    Upgrade to a high-end wash / vac, full detail, or exterior detail for more service options"
+  },
+  {
+    name: "Steam Cleaning Disinfection",
+    base_price: 110,
+    description: "Used on interior panels, steering wheel, front dash, leather seats, exterior. All surface areas that can withstand 175 to 250 degrees heat. Only heat of 175 or greater can kill bacteria and viruses based on CDC guidance.
+
+    Available in details with interiors for $75 dollars, stand alone service price above.
+    Recommend all stream cleaning services have an interior detail to ensure all safety measures take hold.
+    Steam cleaning does not guarantee removal of all dirt."
+  }
+])
