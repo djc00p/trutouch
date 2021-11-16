2ef916f0a63d55238bb0e3b3c5a5dbea7434623a
Deonte Cooper <deonte.cooper12@gmail.com>
2021-11-16 12:57:38 -0700

    Update schema with clear_bra_services

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_185635) do
+ActiveRecord::Schema.define(version: 2021_11_16_193557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clear_bra_services", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_clear_bra_services_on_name", unique: true
+  end
 
   create_table "detail_services", force: :cascade do |t|
     t.string "name"
