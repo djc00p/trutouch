fa91b35c43226d3cc920b42fb27cd583b5c57f9e
Deonte Cooper <deonte.cooper12@gmail.com>
2021-11-28 13:46:32 -0700

    Add test for TintService class methods

--- spec/models/tint_service_spec.rb
+++ spec/models/tint_service_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe TintService, type: :model do
     it { is_expected.to validate_presence_of :price }
     it { is_expected.to validate_presence_of :description }
   end
+
+  describe "Class Methods" do
+    before do
+      create_list(:tint_service, 14)
+    end
+
+    it "display all tint_services" do
+      expect(described_class.tint_services.count).to eq(14)
+    end
+  end
 end
