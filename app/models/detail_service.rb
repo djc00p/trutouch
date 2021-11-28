9a978aa2de116165fc7f3e591c4496d4dfb95b17
Deonte Cooper <deonte.cooper12@gmail.com>
2021-11-28 12:43:48 -0700

    Add Class Methods to DetailService

--- app/models/detail_service.rb
+++ app/models/detail_service.rb
@@ -4,4 +4,34 @@ class DetailService < ApplicationRecord
   validates :name, uniqueness: true
   validates :base_price, presence: true
   validates :description, presence: true
+
+  # Class Methods
+  def self.detail_services
+    all
+  end
+
+  def self.main_detail_services
+    where(name: ["Exterior Detail", "Full Detail", "Interior Detail"])
+      .order(id: :desc)
+  end
+
+  def self.deluxe_detail_services
+    where(name: ["Executive Full Detail", "Presidential Full Detail"])
+  end
+
+  def self.additional_detail_services
+    where.not(
+      name: [
+        "Exterior Detail",
+        "Full Detail",
+        "Interior Detail",
+        "Executive Full Detail",
+        "Presidential Full Detail"
+      ]
+    )
+  end
+
+  def self.select_detail(service)
+    find_by!(name: service)
+  end
 end
