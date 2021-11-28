e89331ce58aa5ec3fcb8a8e7ba2cbd0f903c5845
Deonte Cooper <deonte.cooper12@gmail.com>
2021-11-28 12:44:57 -0700

    Add instance variables to DetailServiceController

--- app/controllers/our_services_controller.rb
+++ app/controllers/our_services_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class OurServicesController < ApplicationController
-  def index; end
+  def index
+    @ds = DetailService
+    @mds = DetailService.main_detail_services
+    @dds = DetailService.deluxe_detail_services
+    @ads = DetailService.additional_detail_services
+    @ts = TintService.all
+    @cbs = ClearBraService.all
+  end
 end
