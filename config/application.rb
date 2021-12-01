1405d4d2f6e062eb36b83ee075a35b57273e264e
Deonte Cooper <deonte.cooper12@gmail.com>
2021-12-01 09:34:48 -0700

    Activating automatic connection switching for database

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,10 @@ module Trutouch
     config.load_defaults 6.0
     config.active_record.writing_role = :default
     config.active_record.reading_role = :readonly
+    config.active_record.database_selector = { delay: 2.seconds }
+    config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+    config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
