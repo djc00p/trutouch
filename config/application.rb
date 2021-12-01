a38950a022072bdd8ca56e39f8fc85f4d8384f21
Deonte Cooper <deonte.cooper12@gmail.com>
2021-12-01 09:27:15 -0700

    Update Database, Application, and Application record with primary and replica databases

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Trutouch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.active_record.writing_role = :default
+    config.active_record.reading_role = :readonly
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
