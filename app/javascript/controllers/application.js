1c108f57f5da8f0a03e98e60b26ae0ff41d4fbf3
Deonte Cooper <deonte.cooper12@gmail.com>
2021-12-04 09:59:25 -0700

    Update JS Controllers for tailwindcss

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,14 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 
-const application = Application.start()
+const application = Application.start();
+
+import { Tabs } from "tailwindcss-stimulus-components";
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+application.debug = false;
+window.Stimulus = application;
+application.register("tabs", Tabs);
+const context = require.context("./controllers", true, /\.js$/);
+Stimulus.load(definitionsFromContext(context));
+export { application };
