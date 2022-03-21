c019c7f0c51387b6044112cc98df4f48e34e6b11
Deonte Cooper <deonte.cooper12@gmail.com>
2022-03-21 12:04:43 -0600

    Add instance method user_activation_email to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :phone_number, :email, presence: true
   validates :phone_number, :email, uniqueness: true # rubocop:disable Rails/UniqueValidationWithoutIndex
   has_secure_password
+
+  def user_activation_email
+    UserMailer.with(user: self).welcome_activation.deliver_now
+  end
 end
