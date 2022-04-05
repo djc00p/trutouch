490c7bc6c74850b4e6f3a7fd9524642d7e6bab16
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-04 23:46:08 -0600

    Add verification and verify actions to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
+  before_action :set_user, only: %i[show edit update destroy verification verify]
 
-  # GET /users/1 or /users/1.json
+  # GET /profile/1 or /profile/1.json
   def show; end
 
-  # GET /users/new
+  # GET /profile/new
   def new
     if current_user
       flash[:danger] = "Already Signed In! Please sign out to create a new account."
@@ -16,43 +16,51 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1/edit
+  # GET /profile/1/edit
   def edit; end
 
-  # POST /users or /users.json
+  # POST /profile or /profile.json
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to TruTouch Auto Detail and Tint #{@user.first_name}!"
-      flash[:info] = "This account has not yet been activated."\
-                     "Please check your email."
-      @user.user_activation_email
-      redirect_to profile_url(@user)
+      flash[:info] = "This account has not yet been verified."\
+                     "Please check for an verification #{@user.prefered_method_of_contact}."
+
+      redirect_to verification_url(@user)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
+  # PATCH/PUT /profile/1 or /profile/1.json
   def update
     redirect_to profile_url(@user) if @user.update(user_params)
 
     flash[:success] = "#{@user.first_name} profile info was successfully updated."
   end
 
-  # DELETE /users/1 or /users/1.json
+  # DELETE /profile/1 or /profile/1.json
   def destroy
     @user.destroy
     flash[:success] = "User was successfully destroyed."
     redirect_to root_path
   end
 
-  # GET /activation
-  def activation
-    user = User.find_by(email: params[:email])
-    redirect_to thank_you_path if user.update(status: "active")
+  # GET /profile/1/verification
+  def verification
+    @user.verify_through_prefered_method_of_contact
+  end
 
-    flash[:success] = "#{user.first_name} your email has been confirmed"
+  # PATCH /profile/1/verify
+  def verify
+    verification_code = user_params[:verification_code]
+
+    return verification_url(@user) unless @user.verification_code == verification_code.to_i
+
+    redirect_to thank_you_path if @user.update(status: "verified", verification_code: nil)
+
+    flash[:success] = "#{@user.first_name} your account has been verified through #{@user.prefered_method_of_contact}"
   end
 
   private
@@ -64,6 +72,9 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :phone_number, :email, :password)
+    params.require(:user).permit(
+      :first_name, :last_name, :phone_number, :email, :password,
+      :prefered_method_of_contact, :verification_code
+    )
   end
 end
