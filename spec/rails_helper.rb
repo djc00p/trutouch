431f18f8ebbfedbcb7e108acdbc6da0941e0177e
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-04 23:17:54 -0600

    Add test helper for Twilio

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,7 @@ require "capybara/rails"
 require "support/factory_bot"
 require "simplecov"
 SimpleCov.start
+SmsSpec.driver = :"twilio-ruby"
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
