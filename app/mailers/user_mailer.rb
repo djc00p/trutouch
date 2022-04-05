6a9f20687f28ee76f9a9a3cc4b886f7742367a96
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-04 23:43:32 -0600

    Update mailer to welcome_verification

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   default from: "notifications@example.com"
 
-  def welcome_activation
+  def welcome_verification
     @user = params[:user]
     mail(to: @user.email, subject: "Welcome To TruTouch")
   end
