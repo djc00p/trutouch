1e7b21202e87c11aa22f43602c076f6da30c70dc
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-04 23:15:19 -0600

    Update factories to use valid phone numbers

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    phone_number { Faker::PhoneNumber.cell_phone }
+    phone_number { ["+14123736103", "+16699168802", "+18082347067", "+15156277272"].shuffle.pop }
     sequence(:email) { |n| "example#{n}@email.com" }
     password { "MyString123" }
     prefered_method_of_contact { "email" }
