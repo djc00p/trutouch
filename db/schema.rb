d2f6969b5e2f560fea2981eea3eb619ed8318860
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-18 11:17:43 -0600

    Update schema with vehicles class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_04_04_210643) do
+ActiveRecord::Schema[7.1].define(version: 2022_04_18_163957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +85,18 @@ ActiveRecord::Schema[7.1].define(version: 2022_04_04_210643) do
     t.string "prefered_method_of_contact"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "make"
+    t.string "model"
+    t.string "production_starting_year"
+    t.string "production_ending_year"
+    t.string "vehicle_size"
+    t.string "vehicle_type"
+    t.string "vehicle_class"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
