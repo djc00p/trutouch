d892e1bf079896b3cf8f174e2691831ebd218479
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-22 18:43:40 -0600

    Update Schema with :vehicles changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_04_18_180834) do
+ActiveRecord::Schema[7.1].define(version: 2022_04_23_002147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -103,11 +103,14 @@ ActiveRecord::Schema[7.1].define(version: 2022_04_18_180834) do
     t.string "color"
     t.string "make"
     t.string "model"
-    t.string "class"
+    t.string "classification"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "vehicles", "users"
 end
