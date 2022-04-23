1c3c317ea8d425e090673d282139d40aa9da2ed6
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-22 18:39:34 -0600

    Update vehicle validations test

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Vehicle, type: :model do
   describe "Validations" do
     it { is_expected.to belong_to :user }
     it { is_expected.to validate_presence_of :year }
+    it { is_expected.to validate_presence_of :color }
     it { is_expected.to validate_presence_of :make }
     it { is_expected.to validate_presence_of :model }
-    it { is_expected.to validate_presence_of :color }
-    it { is_expected.to validate_presence_of :class }
+    it { is_expected.to validate_presence_of :classification }
   end
 end
