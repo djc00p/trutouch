4ef8f13c46ad68cbae052bddc3d6e50b33c47c87
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-25 10:38:15 -0600

    Add routing spec for /my_vehicles

--- spec/routing/vehicles_routing_spec.rb
+++ spec/routing/vehicles_routing_spec.rb
@@ -7,5 +7,8 @@ RSpec.describe VehiclesController, type: :routing do
     it "routes to #new" do
       expect(get: "/add_my_vehicle").to route_to("vehicles#new")
     end
+    it "routes to #create" do
+      expect(post: "/my_vehicles").to route_to("vehicles#create")
+    end
   end
 end
