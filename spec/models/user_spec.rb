aa5a93395313e722659c2daf1c66e5a0672d0014
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-26 09:42:10 -0600

    Add have_many association for vehicle

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   describe "Validations" do
+    it { is_expected.to have_many :vehicles }
+    it { is_expected.to validate_length_of :vehicles }
     it { is_expected.to validate_presence_of :first_name }
     it { is_expected.to validate_presence_of :last_name }
     it { is_expected.to validate_presence_of :phone_number }
