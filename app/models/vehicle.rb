c36919579ec9b3bc69e9241dd3abbd57909679b3
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-27 08:47:04 -0600

    Add instance method to find the production_vehicle that matches vehicle models attributes

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,4 +3,9 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
   validates :year, :color, :make, :model, :classification, presence: true
+
+  # Instance Methods
+  def production_vehicle
+    ProductionVehicle.find_by(make: make, model: model, production_year: year)
+  end
 end
