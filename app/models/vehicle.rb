9cb1bdd987cf5f92f6fda2347e764b00ecc79079
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-27 08:49:36 -0600

    Add instance method to create a string of the production_vehicle attributes that make up the vehicles classification

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -8,4 +8,8 @@ class Vehicle < ApplicationRecord
   def production_vehicle
     ProductionVehicle.find_by(make: make, model: model, production_year: year)
   end
+
+  def production_vehicle_classfication(production_vehicle)
+    production_vehicle.attributes.fetch_values("vehicle_size", "vehicle_type", "vehicle_class").compact.join(" ")
+  end
 end
