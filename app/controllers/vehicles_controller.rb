5e90b5a086fdd63a7945b09d1421a905e528e2d3
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-27 09:28:06 -0600

    Update create action to vehicle controller and add vehicle params

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,9 +1,24 @@
+# frozen_string_literal: true
+
 class VehiclesController < ApplicationController
   def new
     @vehicle = Vehicle.new
   end
 
   def create
+    @vehicle = Vehicle.new(**vehicle_params, user_id: current_user.id)
+    @vehicle.assign_classification
+    if @vehicle.save
+      flash[:success] = "Your #{@vehicle.color} #{@vehicle.make} #{@vehicle.model} has been add!"
+      redirect_to profile_url(current_user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:year, :color, :make, :model)
   end
 end
