de4adbba2e944b207987ea336034ce2ed568e91f
Deonte Cooper <deonte.cooper12@gmail.com>
2022-04-30 00:59:53 -0600

    Update views spec for vehicle#new

--- spec/views/vehicles/new.html.erb_spec.rb
+++ spec/views/vehicles/new.html.erb_spec.rb
@@ -3,14 +3,24 @@
 require "rails_helper"
 
 RSpec.describe "vehicles/new", type: :view do
+  let(:user) do
+    create(:user,
+           first_name: "Luke",
+           last_name: "Skywalker",
+           phone_number: "+14123736103",
+           email: "masterjedi@galacticsavior.com",
+           password: "Maythe4thBew/u")
+  end
+
   before do
+    assign(:user, user)
     assign(:vehicle, build(:vehicle))
   end
 
   it "renders new vehicle form" do # rubocop:disable RSpec/ExampleLength
     render
 
-    assert_select "form[action=?][method=?]", my_vehicles_path, "post" do
+    assert_select "form[action=?][method=?]", profile_my_vehicles_url(user), "post" do
       assert_select "input[name=?]", "vehicle[year]"
 
       assert_select "input[name=?]", "vehicle[color]"
