3014e7329f73d79cdd627c03968a2feedfaf1118
Deonte Cooper <deonte.cooper12@gmail.com>
2022-05-07 12:06:03 -0600

    Add spec for vehicle show.html.erb

---
+++ spec/views/vehicles/show.html.erb_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "vehicles/show", type: :view do
+  let(:user) { create(:user) }
+  let(:vehicle) { create(:vehicle, user_id: user.id) }
+
+  before do
+    assign(:vehicle, vehicle)
+
+    render
+  end
+
+  describe "renders attribute" do
+    it "color in <p>" do
+      expect(rendered).to match(/#{vehicle.color}/)
+    end
+
+    it "year in <p>" do
+      expect(rendered).to match(/#{vehicle.year}/)
+    end
+
+    it "make in <p>" do
+      expect(rendered).to match(/#{vehicle.make}/)
+    end
+
+    it "model in <p>" do
+      expect(rendered).to match(/#{vehicle.model}/)
+    end
+
+    it "classification in <p>" do
+      expect(rendered).to match(/#{vehicle.classification}/)
+    end
+  end
+end
