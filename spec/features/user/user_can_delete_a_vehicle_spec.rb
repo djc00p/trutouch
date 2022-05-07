3d0e75e7d1a36037746750a535a24ca3772f1097
Deonte Cooper <deonte.cooper12@gmail.com>
2022-05-07 15:24:19 -0600

    Add spec for user_can_delete_a_vehicle_spec

---
+++ spec/features/user/user_can_delete_a_vehicle_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "User" do
+  let(:user) { create(:user) }
+  let(:vehicles) { create_list(:vehicle, 5, user_id: user.id) }
+
+  before do
+    user.vehicles << vehicles
+    vehicles
+    visit profile_my_vehicles_url(user)
+  end
+
+  describe "vehicles index" do
+    it "can delete a vehicle" do
+      within "#vehicle-#{vehicles[1].id}" do
+        click_button class: "btn-danger"
+      end
+
+      expect(page).to have_content("Your vehicle has been deleted!")
+    end
+  end
+
+  describe "vehicles show" do
+    it "can delete a vehicle" do
+      within "#vehicle-#{vehicles[1].id}" do
+        click_button "Show"
+      end
+      
+      click_button "Delete Vehicle"
+
+      expect(page).to have_content("Your vehicle has been deleted!")
+    end
+  end
+end
