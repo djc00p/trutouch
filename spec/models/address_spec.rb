668d358be63655ee524c7dd89182ad3ec74e031d
Deonte Cooper <deonte.cooper12@gmail.com>
2022-06-10 09:14:49 -0600

    Add class method spec for us_states

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe Address, type: :model do
   describe "Validations" do
-    it { is_expected.to validate_presence_of :address_line_1 }
+    it { is_expected.to validate_presence_of :address_line1 }
     it { is_expected.to validate_presence_of :city }
     it { is_expected.to validate_presence_of :state }
     it { is_expected.to validate_presence_of :zip_code }
@@ -12,5 +12,11 @@ RSpec.describe Address, type: :model do
 
   describe "Relationships" do
     it { is_expected.to belong_to :user }
+  end
+
+  describe "Class Methods" do
+    it "has list of US States with a count of 50" do
+      expect(described_class.us_states.count).to eq(50)
+    end
   end
 end
