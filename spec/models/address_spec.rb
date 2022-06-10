9780fe8261d4d94515ec00856570db69871db44c
Deonte Cooper <deonte.cooper12@gmail.com>
2022-06-10 08:55:38 -0600

    Add relationship spec for addresses to belong to a user

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe Address, type: :model do
     it { is_expected.to validate_presence_of :state }
     it { is_expected.to validate_presence_of :zip_code }
   end
+
+  describe "Relationships" do
+    it { is_expected.to belong_to :user }
+  end
 end
