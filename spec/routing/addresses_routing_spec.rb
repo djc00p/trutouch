7521cd0d8c584fa1f04c1888d0d5a6b5a42b6108
Deonte Cooper <deonte.cooper12@gmail.com>
2022-06-11 11:06:22 -0600

    Edit spec ot better reflect desired routes

--- spec/routing/addresses_routing_spec.rb
+++ spec/routing/addresses_routing_spec.rb
@@ -5,19 +5,20 @@ require "rails_helper"
 RSpec.describe AddressesController, type: :routing do
   describe "routing" do
     let(:user) { create(:user) }
+    let(:address) { create(:address, user_id: user.id) }
 
     it "routes to #create" do
       expect(post: "/profile/#{user.id}/addresses").to route_to(controller: "addresses", action: "create",
-                                                                  profile_id: user.id.to_s)
+                                                                profile_id: user.id.to_s)
     end
 
     it "routes to #new" do
-      expect(get: "/profile/#{user.id}/add_address").to route_to(controller: "addresses", action: "new",
-                                                                    profile_id: user.id.to_s)
+      expect(get: "/profile/#{user.id}/addresses/new").to route_to(controller: "addresses", action: "new",
+                                                                   profile_id: user.id.to_s)
     end
 
     it "routes to #destroy" do
-      expect(delete: "/addresses/1").to route_to("addresses#destroy", id: "1")
+      expect(delete: "/addresses/#{address.id}").to route_to("addresses#destroy", id: address.id.to_s)
     end
   end
 end
