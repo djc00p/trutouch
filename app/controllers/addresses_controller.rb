ae8e0a77fd529576cd956d38c0c98f9228c9c28f
Deonte Cooper <deonte.cooper12@gmail.com>
2022-07-06 11:24:06 -0600

    Add create action for address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -5,7 +5,17 @@ class AddressesController < ApplicationController
     @address = @user.addresses.build
   end
 
-  def create; end
+  def create
+    address_results = address_search(params[:address_line1])
+    address = @user.addresses.build(address_results)
+    if address.save
+      flash[:success] = "Your address has been add!"
+      redirect_to profile_url(@user)
+    else
+      flash[:danger] = "Please check the provided address is correct."
+      render :new, status: :temporary_redirect
+    end
+  end
 
   def destroy; end
 end
