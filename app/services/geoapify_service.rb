22c90094d14dd6a9b76d04c1d7d4a8b9144f5d41
Deonte Cooper <deonte.cooper12@gmail.com>
2022-07-16 08:14:09 -0600

    Add Faraday connection to geoapify geocode search API

--- app/services/geoapify_service.rb
+++ app/services/geoapify_service.rb
@@ -4,6 +4,15 @@ class GeoapifyService
   private
 
   def conn
-    
+    lat_lon = "#{ENV['trutouch_lat']},#{ENV['trutouch_lon']}"
+    radius_meters = ENV["geoapify_radius_meters_limit"]
+
+    Faraday.new(url: "https://api.geoapify.com/v1/geocode/search",
+                headers: { "Accept" => "application/json" }) do |f|
+      f.params[:limit] = 3
+      f.params[:filter] = "circle:#{lat_lon},#{radius_meters}|countrycode:us"
+      f.params[:apiKey] = ENV["GEOAPIFY_API_KEY"]
+      f.adapter Faraday.default_adapter
+    end
   end
 end
