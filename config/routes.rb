18608af0e28640d8c9c1a7dfe34cb6b7f54e8836
Deonte Cooper <deonte.cooper12@gmail.com>
2022-07-19 16:57:46 -0600

    Add route to confirm address is correct

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     get '/add_my_vehicle', to: 'vehicles#new', as: 'add_my_vehicle'
     resources :addresses, except: [:index, :show, :edit, :update], shallow: true
   end
+  patch '/addresses/:id/confirm', to: 'addresses#confirm', as: 'confirm_address'
+
   get '/sign_up', to: 'users#new', as: 'sign_up'
 
   # User Activation
