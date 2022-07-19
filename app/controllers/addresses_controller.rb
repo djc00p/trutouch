572ad60cba540acbaf2d7a85c5b77dad90e79cc3
Deonte Cooper <deonte.cooper12@gmail.com>
2022-07-18 20:22:00 -0600

    Add address validation method

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,20 +2,24 @@
 
 class AddressesController < ApplicationController
   before_action :find_user, only: %i[new create]
+  before_action :set_address, only: %i[destroy]
 
   def new
     @address = @user.addresses.build
   end
 
   def create
-    address_results = address_search
-    address = @user.addresses.build(address_results)
+    address = @user.addresses.build
+    address_search(address)
+
     if address.save
       flash[:success] = "Your address has been add!"
       redirect_to profile_url(@user)
     else
-      flash[:danger] = "Please check the provided address is correct."
-      render :new, status: :temporary_redirect
+      flash[:danger] =
+        "#{address_params.values.join(', ')} could not be found or is outside of our pickup/dropoff radius.
+        Please check the  provided address is correct."
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -23,17 +27,32 @@ class AddressesController < ApplicationController
 
   private
 
+  def find_user
+    @user = User.includes(:addresses).find(params[:profile_id])
+  end
+
+  def set_address
+    @address = Address.find(params[:id])
+  end
+
   def address_params
     params.require(:address).permit(:address_line1, :city, :state, :zip_code)
   end
 
-  def find_user
-    @user = User.find(params[:profile_id])
-  end
-
-  def address_search
-    search_results = Address.search_results(address_params.values.join(", "))
+  def address_search(address)
+    search_results = address.search_results(address_params.values.join(", "))
 
     address_validation(search_results)
+  end
+
+  def address_validation(results)
+    case results[:validation]
+    when "PARTIALLY_CONFIRMED"
+      flash[:warning] =
+        "Your address has been partially confirmed. Potential cause is
+      \"#{results.delete(:validation_details).titleize}\". Please confirm the found address is correct."
+    when "CONFIRMED"
+      flash[:success] = "Your address has been confirmed!"
+    end
   end
 end
