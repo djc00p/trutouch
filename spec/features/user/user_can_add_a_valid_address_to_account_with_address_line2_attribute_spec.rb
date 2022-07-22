42f6ad8f3cd88f21bea05d0966944d0bbe991173
Deonte Cooper <deonte.cooper12@gmail.com>
2022-07-21 22:57:56 -0600

    Add feature tests for user for coverage

---
+++ spec/features/user/user_can_add_a_valid_address_to_account_with_address_line2_attribute_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "User" do
+  let(:user) { create(:user) }
+  let(:valid_address) { "1750 Welton Street, Denver, CO 80202" }
+  let(:addresses) { create_list(:address, 2, user_id: user.id) }
+
+  before do |test|
+    user
+    visit sign_in_path
+    fill_in "session[email]", with: user.email
+    fill_in "session[password]", with: user.password
+    click_button "Sign In"
+    click_on "Add Address" unless test.metadata[:dont_click_on]
+  end
+
+  it "is able to see new address add that uses 2 address lines", :vcr do
+    fill_in "address[address_line1]", with: valid_address
+
+    click_button "Create Address"
+
+    expect(page).to have_content("Grand Hyatt Denver, 1750 Welton Street, Denver, CO 80202")
+  end
+end
