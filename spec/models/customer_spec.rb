2663a07c16a19571d003d9cf97d5ab06fb107dc5
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-04 11:26:49 -0600

    Add Customer model validations tests

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Customer, type: :model do
+  describe "Validations" do
+    it { is_expected.to have_one :user }
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :phone_number }
+    it { is_expected.to validate_uniqueness_of :phone_number }
+    it { is_expected.to validate_uniqueness_of :email }
+    it { is_expected.to validate_presence_of :prefered_method_of_contact }
+  end
+end
