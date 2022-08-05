0c07e02a79167dd205047e07fd28323bf4e35a64
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-05 07:44:34 -0600

    Add validations for the model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,7 @@
 
 class Customer < ApplicationRecord
   has_one :user
+  validates :first_name, :last_name, :email, :phone_number, :prefered_method_of_contact, presence: true
+  validates :phone_number, :email, uniqueness: true
+  validates :phone_number, phone: true
 end
