62e566143992b8fa781963e5519a97023ae11bc4
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-05 09:25:16 -0600

    Add test for customer to have many vehicles

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,6 +5,7 @@ require "rails_helper"
 RSpec.describe Customer, type: :model do
   describe "Validations" do
     it { is_expected.to have_one :user }
+    it { is_expected.to have_many :vehicles }
     it { is_expected.to validate_presence_of :first_name }
     it { is_expected.to validate_presence_of :last_name }
     it { is_expected.to validate_presence_of :email }
