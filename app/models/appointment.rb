a259dbf2c989354b7c972a779435dfa5ab7346da
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-08 08:05:16 -0600

    Add appointment validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Appointment < ApplicationRecord
+  # Model Associations
   belongs_to :customer
   belongs_to :vehicle
+
+  # Validations
+  validates :estimated_total_cost, :estimated_total_time, :pickup_drop_off, :scheduled_for,
+            :scheduled_on, :services_count, :status, presence: true
 end
