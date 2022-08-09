0b21b26ff230d5fd548642fae77633241a2ec4a0
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-09 08:37:56 -0600

    Add appointment_address spec

---
+++ spec/models/appointment_address_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe AppointmentAddress, type: :model do
+  describe "Model Associations" do
+  end
+
+  describe "Validations" do
+  end
+end
