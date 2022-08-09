92b20e9521980a64e67d73b332eaecf3b98eec05
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-09 00:27:08 -0600

    Add counter culture to track services_count

--- app/models/appointment_service.rb
+++ app/models/appointment_service.rb
@@ -4,6 +4,7 @@ class AppointmentService < ApplicationRecord
   # Model Associations
   belongs_to :appointment
   belongs_to :service, polymorphic: true
+  counter_culture :appointment, column_name: "services_count"
 
   # Validations
   validates :started_at, :estimated_cost, :estimated_time_to_complete, :special_instructions,
