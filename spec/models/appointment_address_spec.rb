d87c84542dcfdf03fbf7f55d7f40de26768804d2
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-09 12:07:49 -0600

    Add AppointmentAddress model associations tests

--- spec/models/appointment_address_spec.rb
+++ spec/models/appointment_address_spec.rb
@@ -4,6 +4,8 @@ require "rails_helper"
 
 RSpec.describe AppointmentAddress, type: :model do
   describe "Model Associations" do
+    it { is_expected.to belong_to :address }
+    it { is_expected.to belong_to :appointment }
   end
 
   describe "Validations" do
