51feb0da20ba2e0e8f5642f08adceaaa6e5f702e
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-09 00:25:37 -0600

    Add has_many associations to appointment class

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,6 +4,13 @@ class Appointment < ApplicationRecord
   # Model Associations
   belongs_to :customer
   belongs_to :vehicle
+  has_many :appointment_services, dependent: :destroy
+  has_many :detail_services, through: :appointment_services, source: :service,
+                             source_type: "DetailService"
+  has_many :tint_services, through: :appointment_services, source: :service,
+                           source_type: "TintService"
+  has_many :clear_bra_services, through: :appointment_services, source: :service,
+                                source_type: "ClearBraService"
 
   # Validations
   validates :estimated_total_cost, :estimated_total_time, :pickup_drop_off, :scheduled_for,
