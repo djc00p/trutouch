c86053a04decd7da2c3a10f6bb385a0a3fa21ec8
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-08 23:29:28 -0600

    Add validations for appointment_services

--- app/models/appointment_services.rb
+++ app/models/appointment_services.rb
@@ -4,4 +4,8 @@ class AppointmentServices < ApplicationRecord
   # Model Associations
   belongs_to :appointment
   belongs_to :service, polymorphic: true
+
+  # Validations
+  validates :started_at, :estimated_cost, :estimated_time_to_complete, :special_instructions,
+            presence: true
 end
