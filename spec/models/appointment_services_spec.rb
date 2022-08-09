991707c6afe5f45779fd1343c54544e18607e367
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-08 23:22:07 -0600

    Add appointment service spec

---
+++ spec/models/appointment_services_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe AppointmentServices, type: :model do
+  describe "Model Associations" do
+    it { is_expected.to belong_to :appointment }
+    it { is_expected.to belong_to :service }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :started_at }
+    it { is_expected.to validate_presence_of :estimated_cost }
+    it { is_expected.to validate_presence_of :estimated_time_to_complete }
+    it { is_expected.to validate_presence_of :special_instructions }
+  end
+end
