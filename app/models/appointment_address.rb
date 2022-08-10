056d6623b2c6c0d61217a696a29220f291a5f891
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-10 00:04:27 -0600

    Add validations to AppointmentAddress class

--- app/models/appointment_address.rb
+++ app/models/appointment_address.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class AppointmentAddress < ApplicationRecord
+  # Model Associations
   belongs_to :address
   belongs_to :appointment
+
+  # Validations
+  validates :pickup, :drop_off, :distance_units, presence: true
 end
