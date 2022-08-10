91aef14ebe39af2efb292f4e107fbdda34d5b532
Deonte Cooper <deonte.cooper12@gmail.com>
2022-08-09 23:58:45 -0600

    Add has_many relationship for addresses to appointment class

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,6 +4,8 @@ class Appointment < ApplicationRecord
   # Model Associations
   belongs_to :customer
   belongs_to :vehicle
+  has_many :appointment_addresses, dependent: :destroy
+  has_many :addresses, through: :appointment_addresses
   has_many :appointment_services, dependent: :destroy
   has_many :detail_services, through: :appointment_services, source: :service,
                              source_type: "DetailService"
